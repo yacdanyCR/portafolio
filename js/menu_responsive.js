9012caa94687f9fcf254aff1303f45eca43b2117
Yacdany <jack20-21@hotmail.com>
2021-04-29 11:10:55 -0600

    Comienzo de Proyecto con menú responsive

---
+++ js/menu_responsive.js
@@ -0,0 +1,1 @@
+var menu_btn=document.querySelector("#menu-btn"),sidebar=document.querySelector("#sidebar"),container=document.querySelector(".my-container");menu_btn.addEventListener("click",()=>{sidebar.classList.toggle("active-nav"),container.classList.toggle("active-cont")});
