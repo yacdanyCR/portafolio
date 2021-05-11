b072d72ed4e7595b6ae4f7040b00a469c1d68622
Yacdany <jack20-21@hotmail.com>
2021-05-10 20:01:58 -0600

    Cambiar red social facebook

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <body class="bg-light bg-gradient">
 
     <header>
-        <!-- Side-Nav -->
+        
         <div class="side-navbar active-nav d-flex justify-content-between flex-wrap flex-column sidebar" id="sidebar">
             <ul class="nav flex-column text-white w-100">
                 <img src="img/logo/40683814_2024764377585594_7330819088991649792_n.jpg" style="width:100%;" alt="">
@@ -40,19 +40,19 @@
             </ul>
 
             <span href="#" class="nav-link h4 w-100 mb-5 icons-contact">
-                <a href="https://www.facebook.com/BlackHosh/" target="_blank"><i class="bi bi-facebook"></i></a>
+                <a href="https://www.linkedin.com/in/yacdany-arguedas-mora-22592b211?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BSO7dUR3OTk%2B%2BGuxtHweVOQ%3D%3D" target="_blank"><i class="bi bi-linkedin"></i></a>
                 <a href="https://github.com/Jack-CR" target="_blank"><i class="bi bi-github"></i></a>
                 <a href="https://wa.link/wsghi7" target="_blank"><i class="bi bi-whatsapp"></i></a>
             </span>
         </div>
     </header>
-    <!-- Main Wrapper -->
+    
     <div class=" p-1 my-container active-cont">
-        <!-- Top Nav -->
+        
         <nav class="navbar top-navbar navbar-light  px-5">
             <a class="btn border-0 menu-btn" id="menu-btn"><i class="bi bi-list"></i></a>
         </nav>
-        <!--End Top Nav -->
+        
         <div class="container">
 
             <div class="row" data-aos="fade-right" data-aos-duration="2000">
