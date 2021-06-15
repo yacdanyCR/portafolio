9c8ba1d9bca80b9106dcf7aeec94d237eff124db
Yacdany <jack20-21@hotmail.com>
2021-06-14 22:09:12 -0600

    animacion cv

--- index.php
+++ index.php
@@ -272,7 +272,7 @@
                 </div>
             </div>
             <!-- CV  -->
-            <div class="row mt-5 d-flex bg-white bg-gradient border rounded d-flex justify-content-center">
+            <div class="row mt-5 d-flex bg-white bg-gradient border rounded d-flex justify-content-center"data-aos="fade-right" data-aos-duration="2000">
                 <h2>Curriculum</h2>
                 <a name="CV"></a>
                 <div class="embed-responsive embed-responsive-21by9" style="height: 900px;">
