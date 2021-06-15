6443ffc1f48df153e9ea9636f0d3ffa8539bf3c5
Yacdany <jack20-21@hotmail.com>
2021-06-15 00:09:15 -0600

    Estructurando proyecto con php

---
+++ layouts/cv.php
@@ -0,0 +1,8 @@
+<div class="row mt-5 d-flex bg-white bg-gradient border rounded d-flex justify-content-center" data-aos="fade-right" data-aos-duration="2000">
+    <h2>Curriculum</h2>
+    <hr>
+    <a name="CV"></a>
+    <div class="embed-responsive embed-responsive-21by9 mt-4" style="height: 900px;">
+        <iframe style="width: 100%;height:900px;" class="embed-responsive-item" src="CV/YACDANY ARGUEDAS MORA CV.pdf"></iframe>
+    </div>
+</div>
---
+++ layouts/proyectos.php
@@ -0,0 +1,94 @@
+<div class="row mt-5 bg-white bg-gradient border rounded">
+    <h2>Mis Proyectos</h2>
+    <hr>
+    <div class="col-md-6 mt-5 ">
+        <a name="proyectos"></a>
+
+        <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false" data-bs-ride="carousel">
+            <div class="carousel-inner">
+
+                <div class="carousel-item active">
+                    <img src="img/proyectos/proyecto_1/Captura de pantalla (9).png" class="d-block w-100" alt="imagen">
+                </div>
+
+                <div class="carousel-item ">
+                    <img src="img/proyectos/proyecto_1/Captura de pantalla (2).png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/proyectos/proyecto_1/Captura de pantalla (3).png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/proyectos/proyecto_1/Captura de pantalla (4).png" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+                <span aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+                <span aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Práctica Supervisada Restaurante Betico Mata & Parrilla</h5>
+                    <p class="card-text">Desarrollar un sitio para una pymes con una duración de 3 meses para concluir con la capacitación.</p>
+                    <ul>
+                        <li>Panel de Administración para el dueño</li>
+                        <li>Formulario de Contacto</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-6 mt-5">
+        <div id="carouselExampleControlsNoTouching2" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner" data-bs-interval="10000">
+
+                <div class="carousel-item active">
+                    <img src="img/proyectos/proyecto_2/1.png" class="d-block w-100" alt="...">
+                </div>
+
+                <div class="carousel-item">
+                    <img src="img/proyectos/proyecto_2/2.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/proyectos/proyecto_2/3.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/proyectos/proyecto_2/4.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/proyectos/proyecto_2/5.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/proyectos/proyecto_2/6.png" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching2" data-bs-slide="prev">
+                <span aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching2" data-bs-slide="next">
+                <span aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Mini Mercado Libre</h5>
+                    <p class="card-text">Aplicación web donde cada usuario puede publicar sus productos y realizar varias de las funciones de un mercado libre como:</p>
+                    <ul>
+                        <li>Iniciar Sessión</li>
+                        <li>Registrarse</li>
+                        <li>Agregar,Quitar,Eliminar,Actualizar sus Productos</li>
+                        <li>Ver productos de otros Usuarios</li>
+                        <li>Comentar los productos</li>
+                        <li>Formulario de Contacto</li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
